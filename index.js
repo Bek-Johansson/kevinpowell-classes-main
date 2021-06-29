7fa20c21380b7a5865fd89bdec8f9c8e91a36331
Bek Johansson <bekjohansson@icloud.com>
2021-06-29 08:40:34 -0500

    commit

--- index.js
+++
@@ -1,5 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
-navToggleaddEventListener("click", () => {
-  nav.classList.toggle("nav--visible");
-});
